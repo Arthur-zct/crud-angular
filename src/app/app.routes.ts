2f453b8790095fa007c5efae1b1c835cb2504bd1
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-08-08 19:19:17 -0300

    Rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Cadastro } from './cadastro/cadastro';
+import { Consulta } from './consulta/consulta';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Cadastro', component: Cadastro },
+    { path: 'Consulta', component: Consulta }
+];
