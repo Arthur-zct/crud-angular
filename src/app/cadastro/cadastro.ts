f7d7cd2f2070e56afe33fdaaa355d955bb467413
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-08-13 19:35:01 -0300

    Ajustando tamanho e button

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -4,12 +4,14 @@ import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from "@angular/material/form-field";
 import {MatInputModule } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule, MatCardModule, FormsModule, 
-    MatFormFieldModule, MatInputModule],
+  imports: [FlexLayoutModule, MatCardModule, FormsModule,
+    MatFormFieldModule, MatInputModule, MatIcon, MatButtonModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss'
 })
