aed66032e97f37e0d88bf6c8c56154d83d3f423a
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-08-21 18:57:43 -0300

    Criação cliente.ts

---
+++ src/app/cadastro/cliente.ts
@@ -0,0 +1,16 @@
+import {v4 as uuid} from 'uuid';
+
+export class Cliente {
+    id?: string;
+    nome?: string;
+    cpf?: string;
+    dataNascimento?: string;
+    email?: string;
+
+    //criar um cliente vazio
+    static NewClient(): Cliente {
+        let cliente = new Cliente();
+        cliente.id = uuid(); //gera um id unico para o cliente
+        return cliente;
+    }
+}
