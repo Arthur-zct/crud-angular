aa71728be62b88e6907962082926839e96693e2f
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-08-25 19:03:39 -0300

    obterStorage

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -5,7 +5,23 @@ import { Cliente } from './cadastro/cliente';
   providedIn: 'root'
 })
 export class ClienteService {
+
+  static REPO_CLIENTES = "_CLIENTES"; 
+
   salvar(cliente: Cliente) {
     console.log('Cliente salvo:', cliente);
+  }
+
+  obterStorage() : Cliente[] { //cliente é o que vai ser retornado
+    const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
+    if (repositorioClientes) { //caso ja tenha
+      const clientes: Cliente[] = JSON.parse(repositorioClientes); //parse recebe a string repositorioclientes e transforma em um array de clientes (localstorage só aceita string)
+      return clientes;
+    }
+    //caso nao tenha
+    const clientes: Cliente[] = [];
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes)); //cria o item no localstorage
+    return clientes;
+    //retorna um storage vazio
   }
 }
